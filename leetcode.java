2548af4c592d6355dff09f3ab79e59fb91f35096
Utkarsh Rastogi <utkarshrastogiur01@gmail.com>
2025-03-18 16:58:02 +0530

    New repo of leetcode created.

---
+++ leetcode.java
@@ -0,0 +1,25 @@
+
+public class leetcode {
+    public static int longestNiceSubarray(int arr[]){
+        int n = arr.length;
+        int l=0, r=0;
+        int max = 0;
+        int mask = 0;
+        while(r<n){
+            while((mask & arr[r]) != 0){
+                mask^= arr[l];
+                l++;
+            }
+            max = Math.max(max, r-l+1);
+            mask |= arr[r];
+            r++;
+        }
+        
+        return max;
+    }
+        public static void main(String[] args) {
+        int arr[] = {1,3,8,48,10,2,8,48,128};
+        System.out.println(longestNiceSubarray(arr));
+        
+    }
+}
